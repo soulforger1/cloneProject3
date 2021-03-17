38f12288cd7f9f5fd2b7b56a72df44f9dadb6b7a
Zolboo Oktybri <zolboooktybri@Zolboos-MacBook-Pro.local>
2021-03-17 23:47:45 +0800

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View} from 'react-native';
+import {TabNavigation} from './navigations';
+
+export const App = () => {
+  return (
+    <View>
+      <TabNavigation />
+    </View>
+  );
+};
---
+++ src/navigations/tabNavigation.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Time} from '../screens';
+
+export const TabNavigation = () => {
+  return (
+    <View>
+      <Time />
+    </View>
+  );
+};
